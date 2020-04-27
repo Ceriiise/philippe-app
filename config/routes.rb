0b6440a77ef0bc804dbe95736da9f5b4f4ff885e
Philippine Berton <Phi.berton@hotmail.fr>
2020-04-27 17:49:28 +0200

    add administrate (#5)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :bookings
+    resources :items
+
+    root to: "items#index"
+  end
+
   root to: 'items#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :items do
