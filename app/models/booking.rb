b5ee86ec44d55465130280a55c96d5f4e7628020
Philippine Berton <Phi.berton@hotmail.fr>
2020-05-12 15:01:26 +0200

    add gem simple calendar (#7)

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,4 +4,12 @@ class Booking < ApplicationRecord
   validates :user,       presence: :true
   validates :start_date, presence: :true
   validates :end_date,   presence: :true
+
+  def start_time
+      self.start_date ##Where 'start' is a attribute of type 'Date' accessible through MyModel's relationship
+  end
+
+  def end_time
+      self.end_date
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -16,9 +16,13 @@ end
 
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.time_zone = 'Paris'
+    config.beginning_of_week = :monday
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
   end
+
+
 end
