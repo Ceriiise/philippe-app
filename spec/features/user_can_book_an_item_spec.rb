918bb4a44d7c8afd8d8bb9a1739fcf06ba98ede7
Philippine Berton <Phi.berton@hotmail.fr>
2020-05-12 14:01:51 +0200

    fix test

--- spec/features/user_can_book_an_item_spec.rb
+++ spec/features/user_can_book_an_item_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe "User can book an item" do
   let!(:item) { create(:item) }
+  start_date = (Date.today + 1.day)
+  end_date = (Date.today + 3.day)
 
   it "can book an item", js: true do
     visit items_path
@@ -14,8 +16,8 @@ RSpec.describe "User can book an item" do
     fill_in "booking[user]", with: 'Julien'
 
     within ".dayContainer" do
-      find('span[aria-label="April 27, 2020"]').click
-      find('span[aria-label="April 30, 2020"]').click
+      find("span[aria-label='#{start_date.strftime("%B %e, %Y")}']").click
+      find("span[aria-label='#{end_date.strftime("%B %e, %Y")}']").click
     end
 
     click_on "Réserver"
@@ -23,7 +25,7 @@ RSpec.describe "User can book an item" do
     expect(Booking.count).to eq(counter + 1)
 
     expect(page).to have_content("Julien")
-    expect(page).to have_content("2020-04-27")
-    expect(page).to have_content("2020-04-30")
+    expect(page).to have_content("#{start_date}")
+    expect(page).to have_content("#{end_date}")
   end
 end
