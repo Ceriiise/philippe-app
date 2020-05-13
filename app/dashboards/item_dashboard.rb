1cab6345c6ea46ef14ad306d214cd81537c7e1a6
Philippine Berton <Phi.berton@hotmail.fr>
2020-05-13 22:38:05 +0200

    fix colors

--- app/dashboards/item_dashboard.rb
+++ app/dashboards/item_dashboard.rb
@@ -42,7 +42,6 @@ class ItemDashboard < Administrate::BaseDashboard
   # an array of attributes that will be displayed
   # on the model's form (`new` and `edit`) pages.
   FORM_ATTRIBUTES = %i[
-  bookings
   name
   description
   ].freeze
